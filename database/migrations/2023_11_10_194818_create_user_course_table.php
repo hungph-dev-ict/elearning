dcf64718ae1103e7e1d1f2b3873130947dd2248a
htktranghy <htktranghy@gmail.com>
2023-11-11 03:09:30 +0700

    20231111 tranghtk tao file migration user_course

---
+++ database/migrations/2023_11_10_194818_create_user_course_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_course', function (Blueprint $table) {
+            $table->bigInteger('user_id');
+            $table->bigInteger('course_id');
+            $table->primary(['user_id', 'course_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_course');
+    }
+};
